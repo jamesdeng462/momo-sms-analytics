b96371c1a6f702cc1b36363e8cc29795d382f0d2
BYUUSAA <byuussaa@gmail.com>
2026-01-30 20:44:25 +0000

    Update Transaction_Categories Table.sql

---
+++ Transaction_Categories Table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE transaction_categories (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50) UNIQUE NOT NULL,
+    description TEXT,
+    code VARCHAR(10) UNIQUE NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
