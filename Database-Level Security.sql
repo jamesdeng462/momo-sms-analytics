08438eee006a5501efb8aa7869790d17c0bb8de3
BYUUSAA <byuussaa@gmail.com>
2026-01-30 20:52:24 +0000

    Delete Database-Level Security.sql

--- Database-Level Security.sql
+++
@@ -1,7 +0,0 @@
--- Create application user with least privileges
-CREATE USER 'momo_api'@'localhost' IDENTIFIED BY 'SecurePass123!';
-GRANT SELECT, INSERT, UPDATE, DELETE ON momo_sms_db.* TO 'momo_api'@'localhost';
-
--- Create read-only user for reporting
-CREATE USER 'momo_readonly'@'localhost' IDENTIFIED BY 'ReadOnly456!';
-GRANT SELECT ON momo_sms_db.* TO 'momo_readonly'@'localhost';
